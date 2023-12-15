968a6b3ead26987db8628da62b483e2435baab11
Jose Boucourt <joseboucourt@Joses-MBP.attlocal.net>
2023-12-15 09:55:24 -0500

    First Commit

---
+++ src/test/java/java/com/hubspot/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.hubspot.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
